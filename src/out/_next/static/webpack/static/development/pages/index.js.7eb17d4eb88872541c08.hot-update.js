7b7cf64e4665ee450f38440340daa99be0c22828
angelica-silva <angelicasilvaoro@gmail.com>
2019-03-14 23:48:53 -0300

    added initial setup

---
+++ src/out/_next/static/webpack/static/development/pages/index.js.7eb17d4eb88872541c08.hot-update.js
@@ -0,0 +1,38 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "../node_modules/styled-components/dist/styled-components.browser.esm.js");
+var _jsxFileName = "/Users/idp/Development/angelica-silva.github.io/src/pages/index.tsx";
+
+
+var Title = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].h1.withConfig({
+  displayName: "pages__Title",
+  componentId: "sc-1b4b84b-0"
+})(["font-family:sans-serif;"]);
+
+var Index = function Index() {
+  return react__WEBPACK_IMPORTED_MODULE_0__["createElement"](Title, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Hello");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Index);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.7eb17d4eb88872541c08.hot-update.js.map
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-family: sans-serif;
+`
+
+const Index: React.FunctionComponent = () => (
+    <Title>Hello</Title>
+)
+
+export default Index
